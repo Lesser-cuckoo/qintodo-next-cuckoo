fbd5d56771c816880491377eed5591827a2b87f2
りーべ <magical.liebe@gmail.com>
2022-02-15 18:59:08 +0900

    :sparkles: タスク追加・表示の実装

--- src/lib/SupabaseClient.ts
+++ src/lib/SupabaseClient.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import type { TaskType } from "src/lib/Datetime";
+import { getDate, getDateEnd } from "src/lib/Datetime";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
@@ -11,3 +13,54 @@ if (!SUPABASE_KEY) {
 }
 
 export const client = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export type TodoType = {
+  id: number;
+  uid: string;
+  inserted: Date;
+  task: string;
+  deadline: Date | null;
+  isComplete: boolean;
+};
+
+export const addTodo = async (
+  uid: string,
+  task: string,
+  taskType: TaskType
+) => {
+  const { error } = await client
+    .from("todos")
+    .insert([{ uid: uid, task: task, deadline: getDate(taskType) }]);
+  if (error) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const getTodo = async (taskType: TaskType) => {
+  if (taskType != "other") {
+    const start = getDate(taskType);
+    const end = getDateEnd(taskType);
+    const { data, error } = await client
+      .from<TodoType>("todos")
+      .select("*")
+      .gte("deadline", start.toISOString())
+      .lte("deadline", end.toISOString());
+    if (error || !data) {
+      return [];
+    } else {
+      return data;
+    }
+  } else {
+    const { data, error } = await client
+      .from<TodoType>("todos")
+      .select("*")
+      .is("deadline", null);
+    if (error || !data) {
+      return [];
+    } else {
+      return data;
+    }
+  }
+};
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,182 @@
+import { Auth } from "@supabase/ui";
 import type { VFC } from "react";
-import { Button } from "src/component/Button";
+import { useCallback } from "react";
+import { useEffect, useState } from "react";
+import { HiPlusCircle } from "react-icons/hi";
+import type { TodoType } from "src/lib/SupabaseClient";
+import { addTodo, getTodo } from "src/lib/SupabaseClient";
 
 export const Index: VFC = () => {
-  const handleClick = () => {
-    window.alert("Hello, World!");
-  };
+  const { user } = Auth.useUser();
+  const [textToday, setTextToday] = useState<string>("");
+  const [textTomorrow, setTextTomorrow] = useState<string>("");
+  const [textOther, setTextOther] = useState<string>("");
+  const [todoToday, setTodoToday] = useState<TodoType[]>([]);
+  const [todoTomorrow, setTodoTomorrow] = useState<TodoType[]>([]);
+  const [todoOther, setTodoOther] = useState<TodoType[]>([]);
+
+  const handleChangeTextToday = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTextToday(e.target.value);
+    },
+    []
+  );
+
+  const handleChangeTextTomorrow = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTextTomorrow(e.target.value);
+    },
+    []
+  );
+
+  const handleChangeTextOther = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTextOther(e.target.value);
+    },
+    []
+  );
+
+  const updateTodo = useCallback(async () => {
+    let data = await getTodo("today");
+    setTodoToday(data);
+    data = await getTodo("tomorrow");
+    setTodoTomorrow(data);
+    data = await getTodo("other");
+    setTodoOther(data);
+  }, []);
+
+  const handleAddToday = useCallback(async () => {
+    if (textToday && user) {
+      const uid = user.id;
+      const isSuccess = await addTodo(uid, textToday, "today");
+      if (isSuccess) {
+        updateTodo();
+        setTextToday("");
+      } else {
+        alert("タスクの追加に失敗しました");
+      }
+    }
+  }, [textToday, user, updateTodo]);
+
+  const handleAddTomorrow = useCallback(async () => {
+    if (textTomorrow && user) {
+      const uid = user.id;
+      const isSuccess = await addTodo(uid, textTomorrow, "tomorrow");
+      if (isSuccess) {
+        updateTodo();
+        setTextTomorrow("");
+      } else {
+        alert("タスクの追加に失敗しました");
+      }
+    }
+  }, [textTomorrow, user, updateTodo]);
+
+  const handleAddOther = useCallback(async () => {
+    if (textOther && user) {
+      const uid = user.id;
+      const isSuccess = await addTodo(uid, textOther, "other");
+      if (isSuccess) {
+        updateTodo();
+        setTextOther("");
+      } else {
+        alert("タスクの追加に失敗しました");
+      }
+    }
+  }, [textOther, user, updateTodo]);
+
+  useEffect(() => {
+    updateTodo();
+  }, [updateTodo]);
 
   return (
-    <div>
-      <h2>Index</h2>
-      <Button tag="button" className="p-2" onClick={handleClick}>
-        Click me!
-      </Button>
+    <div className="grid grid-cols-3 gap-4 my-8 mx-12">
+      <div className="col-span-1">
+        <div className="text-xl font-bold text-[#F43F5E]">今日する</div>
+        <div className="mt-6">
+          {todoToday.map((item) => (
+            <div
+              key={`item-${item.id}`}
+              className="flex gap-3 justify-start p-1"
+            >
+              <div className="aspect-square h-5 rounded-full border-2 border-[#C2C6D2]"></div>
+              <div className="h-5">{item.task}</div>
+            </div>
+          ))}
+          <div className="flex justify-start p-1">
+            <HiPlusCircle size={20} className="text-[#C2C6D2]" />
+            <input
+              type="text"
+              value={textToday}
+              onChange={handleChangeTextToday}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleAddToday();
+                }
+              }}
+              className="h-5 placeholder:text-[#C2C6D2] border-0 focus:ring-0 caret-[#F43F5E]"
+              placeholder="タスクを追加する"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-span-1">
+        <div className="text-xl font-bold text-[#FB923C]">明日する</div>
+        <div className="mt-6">
+          {todoTomorrow.map((item) => (
+            <div
+              key={`item-${item.id}`}
+              className="flex gap-3 justify-start p-1"
+            >
+              <div className="aspect-square h-5 rounded-full border-2 border-[#C2C6D2]"></div>
+              <div className="h-5">{item.task}</div>
+            </div>
+          ))}
+          <div className="flex justify-start p-1">
+            <HiPlusCircle size={20} className="text-[#C2C6D2]" />
+            <input
+              type="text"
+              value={textTomorrow}
+              onChange={handleChangeTextTomorrow}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleAddTomorrow();
+                }
+              }}
+              className="h-5 placeholder:text-[#C2C6D2] border-0 focus:ring-0 caret-[#F43F5E]"
+              placeholder="タスクを追加する"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-span-1">
+        <div className="text-xl font-bold text-[#FBBF24]">今度する</div>
+        <div className="mt-6">
+          {todoOther.map((item) => (
+            <div
+              key={`item-${item.id}`}
+              className="flex gap-3 justify-start p-1"
+            >
+              <div className="aspect-square h-5 rounded-full border-2 border-[#C2C6D2]"></div>
+              <div className="h-5">{item.task}</div>
+            </div>
+          ))}
+          <div className="flex justify-start p-1">
+            <HiPlusCircle size={20} className="text-[#C2C6D2]" />
+            <input
+              type="text"
+              value={textOther}
+              onChange={handleChangeTextOther}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleAddOther();
+                }
+              }}
+              className="h-5 placeholder:text-[#C2C6D2] border-0 focus:ring-0 caret-[#F43F5E]"
+              placeholder="タスクを追加する"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
