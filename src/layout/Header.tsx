d04a52674ab7dd22adcabc49f29afc40233b2a28
yamadahiroki <hirockysan1983@gmail.com>
2022-02-16 21:17:48 +0900

    ✨ 不要なdivを削除

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,16 +5,14 @@ import type { VFC } from "react";
  * @package
  */
 export const Header: VFC = () => (
-  <div>
-    <div className="flex justify-around items-center">
-      <Image src="/Qin_todo.png" alt="logo" width={160} height={80} />
-      <Image
-        src="/avatar.png"
-        alt="avatarImage"
-        width={30}
-        height={30}
-        layout="fixed"
-      />
-    </div>
+  <div className="flex justify-around items-center">
+    <Image src="/Qin_todo.png" alt="logo" width={160} height={80} />
+    <Image
+      src="/avatar.png"
+      alt="avatarImage"
+      width={30}
+      height={30}
+      layout="fixed"
+    />
   </div>
 );
