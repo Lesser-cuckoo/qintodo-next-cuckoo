5e8e14701987621a0aeb3aba84a28527c92ce33c
hiroky1983 <hirockysan1983@gmail.com>
2022-02-17 15:56:37 +0900

    アイコンの表示

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -137,7 +137,7 @@ export const Index: VFC = () => {
                 >
                   <div className="aspect-square h-5 rounded-full border-2 border-[#C2C6D2]"></div>
                   <p className="grow h-5">{item.task}</p>
-                  <div className="flex invisible hover:visible gap-2 items-center mr-6 text-[#C2C6D2] hover:cursor-pointer">
+                  <div className="flex gap-2 items-center mr-6 text-[#C2C6D2] hover:cursor-pointer">
                     <MdOutlineContentCopy />
                     <CgTrash />
                   </div>
