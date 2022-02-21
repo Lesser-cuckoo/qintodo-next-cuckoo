2f7592c3081b0aed36f77885848b82f2a2cd322c
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-21 17:29:43 +0900

    ➕  tailwindテーマカラー追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        today: "#F43F5E",
+        tomorrow: "#FB923C",
+        other: "#FBBF24",
+      },
+    },
+  },
   plugins: [
-    require("@tailwindcss/forms"),
+    require("daisyui"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
   ],
