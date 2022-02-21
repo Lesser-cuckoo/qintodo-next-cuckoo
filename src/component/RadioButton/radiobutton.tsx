514a603fe473d91689635467b348acebefc41876
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-21 17:32:56 +0900

    💄  UIを修正

--- src/component/RadioButton/radiobutton.tsx
+++ src/component/RadioButton/radiobutton.tsx
@@ -17,7 +17,7 @@ export const RadioButton2: VFC<Style> = (props) => {
   return (
     <>
       <div
-        className={`flex justify-center p-[0.15rem] w-5 h-5 rounded-full ring-2 ring-gray-200 mr-1`}
+        className={`flex justify-center w-5 h-5 rounded-full ring-2 ring-gray-200 mr-3`}
         onClick={handleJudgeCompleted}
       >
         <button
