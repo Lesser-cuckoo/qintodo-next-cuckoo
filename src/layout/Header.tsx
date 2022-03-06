42d63f3bba7caa7fd82a5792c932cb06e5fbb8fa
katan4722 <uver7770422@gmail.com>
2022-03-07 01:34:37 +0900

    ✨ 設定画面用のPopover作成

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,6 +2,7 @@ import { Auth } from "@supabase/ui";
 import Image from "next/image";
 import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
+import { HiOutlineLogout } from "react-icons/hi";
 import { Avatar } from "src/component/Avatar";
 import { addNewProfile, getProfile } from "src/lib/SupabaseClient";
 
@@ -10,13 +11,16 @@ import { addNewProfile, getProfile } from "src/lib/SupabaseClient";
  */
 export const Header = () => {
   const { user } = Auth.useUser();
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
   const [avatar, setAvatar] = useState<string>("");
+  const [name, setName] = useState<string>("");
 
   const fetchProfile = useCallback(async (uid: string) => {
     const profile = await getProfile();
     if (profile) {
       setAvatar(profile.avatar);
+      setName(profile.username);
     } else {
       const isOk = await addNewProfile(uid);
       if (!isOk) {
@@ -31,24 +35,50 @@ export const Header = () => {
     }
   }, [user, fetchProfile]);
 
+  const handleOpenMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
+
   return (
-    <div className="flex justify-around items-center p-5">
-      <Link href="/" passHref>
-        <a>
-          <Image
-            className="cursor-pointer"
-            src="/qintodo_logo.svg"
-            alt="QinTodo"
-            width={150}
-            height={50}
-          />
-        </a>
-      </Link>
-      <Link href="/mypage" passHref>
-        <a>
-          <Avatar image={avatar} size="small" isRounded={true} />
-        </a>
-      </Link>
-    </div>
+    <>
+      <div className="flex justify-around items-center p-5 dark:bg-[#353e49]">
+        <Link href="/" passHref>
+          <a>
+            <Image
+              className="cursor-pointer"
+              src="/qintodo_logo.svg"
+              alt="QinTodo"
+              width={150}
+              height={50}
+            />
+          </a>
+        </Link>
+        {/* <Link href="/mypage" passHref> */}
+        <div className="relative w-80 text-center">
+          <button onClick={handleOpenMenu}>
+            <Avatar image={avatar} size="small" isRounded={true} />
+          </button>
+          {isOpenMenu ? (
+            <div className="absolute shadow-xl card bg-base-100">
+              <div className="flex flex-col gap-4 p-5 w-80">
+                <div className="flex gap-2 items-center ">
+                  <button onClick={handleOpenMenu} className="max-w-fit">
+                    <Avatar image={avatar} size="small" isRounded={true} />
+                  </button>
+                  <div className="text-sm text-left">
+                    <p>{name}</p>
+                    <p>@ahsuidhais</p>
+                  </div>
+                </div>
+                <button className="items-center text-xs font-bold text-red-400 card-actions">
+                  <HiOutlineLogout size={19} />
+                  <p>ログアウト</p>
+                </button>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </>
   );
 };
